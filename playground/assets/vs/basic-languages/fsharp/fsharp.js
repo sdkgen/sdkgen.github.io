421d3b6744d338f35773813504165b736f72f93f
dgadelha <dgadelha@users.noreply.github.com>
2026-06-09 16:24:17 +0000

    deploy release

--- playground/assets/vs/basic-languages/fsharp/fsharp.js
+++ playground/assets/vs/basic-languages/fsharp/fsharp.js
@@ -1,6 +1,6 @@
 "use strict";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.43.0(94c055bcbdd49f04a0fa15515e848542a79fb948)
+ * Version: 0.45.0(5e5af013f8d295555a7210df0d5f2cea0bf5dd56)
  * Released under the MIT license
  * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
  *-----------------------------------------------------------------------------*/
